c4e341827e2aa3acbd34faf575778554fb2fe8f5
muhammad <muhammadgulyamov14.14@gmail.com>
2022-09-08 13:17:16 +0300

    homework

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -3,3 +3,11 @@
 #Find the reverse of the number and assign it to a variable called 'answer'.
 
 #Print the answer variable
+number = 82 
+x2  = number%10
+number = number//10 
+
+x1 = number%10 
+number = number
+
+print(x2*10+x1)
