0d3ea5abbb77b6614038874e2336d39281f1746d
muhammad <muhammadgulyamov14.14@gmail.com>
2022-09-08 12:17:18 +0300

    homework02

--- arithmetic_op02.py
+++ arithmetic_op02.py
@@ -3,3 +3,12 @@
 #Create a variable 'answer' and assign it the sum of the number's digits.
 
 #Print answer.
+number = 43 
+x1 = number%10
+number=number//10
+
+x2 = number%10
+number=number//10
+
+answer= x1 + x2
+print(answer)
